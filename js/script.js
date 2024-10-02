b4bb7f18895030f391ec4f6d0daa80085ea6d5cb
SterrenHeer <sterrenheer@gmail.com>
2024-10-02 14:18:38 +0300

    add test locations block and some icons for his section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+let scale = 1;
+let locations = document.querySelector('.locations_wrapper');
+document.querySelector('#plus').addEventListener('click', function () {
+    scale += 0.05;
+    locations.style.transform = `scale(${scale})`;
+});
+document.querySelector('#minus').addEventListener('click', function () {
+    scale -= 0.05;
+    locations.style.transform = `scale(${scale})`;
+});
+document.querySelectorAll('.marker').forEach((marker) => {
+    marker.style.top = marker.dataset.y + '%';
+    marker.style.left = marker.dataset.x + '%';
+});
