9c1b763f6be18440663836a759b65046cf41ad06
SterrenHeer <sterrenheer@gmail.com>
2024-10-09 11:28:23 +0300

    refine statistics page with locations block

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 let scale = 1;
 let locations = document.querySelector('.locations_wrapper');
-document.querySelector('#plus').addEventListener('click', function () {
-    scale += 0.05;
-    locations.style.transform = `scale(${scale})`;
-});
-document.querySelector('#minus').addEventListener('click', function () {
-    scale -= 0.05;
-    locations.style.transform = `scale(${scale})`;
-});
+
+if (document.querySelector('.locations .buttons') != null) {
+    document.querySelector('#plus').addEventListener('click', function () {
+        scale += 0.05;
+        locations.style.transform = `scale(${scale})`;
+    });
+    document.querySelector('#minus').addEventListener('click', function () {
+        scale -= 0.05;
+        locations.style.transform = `scale(${scale})`;
+    });
+}
+
 document.querySelectorAll('.marker').forEach((marker) => {
     marker.style.top = marker.dataset.y + '%';
     marker.style.left = marker.dataset.x + '%';
@@ -44,10 +48,12 @@ dots.forEach((dot, index) => {
     dot.addEventListener('click', (e) => {
         dots.forEach(dot => dot.classList.remove('active'));
         dot.classList.add('active');
-        if (dot.classList.contains('marker')) {
-            dots[index + len].classList.add('active')
-        } else {
-            dots[index - len].classList.add('active')
+        if (document.querySelector('.video .buttons') != null) {
+            if (dot.classList.contains('marker')) {
+                dots[index + len].classList.add('active')
+            } else {
+                dots[index - len].classList.add('active')
+            }
         }
     });
 });
